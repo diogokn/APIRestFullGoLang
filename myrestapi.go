f56b913aa14f5abad5fa112e9e811432c988fcc8
Diogo Namie <41928681+diogokn@users.noreply.github.com>
2021-12-10 20:58:30 -0300

    Add files via upload

---
+++ myrestapi.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+var Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Article{
+		Title:   "Medico",
+		Desc:    "teste",
+		Content: "observação",
+	}
+
+	fmt.Println("Endpoint Hit: allArticles")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+	handleRequests()
+}
